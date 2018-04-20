65eba5c6a63c4a8a3ed0f41ef0444acd674cdd6d
Hoskins <ACR14@CORP.UPC>
2018-04-19 20:22:44 -0400

    troubleshooting require error

--- src/playground/es6-arrow-function-2.js
+++ src/playground/es6-arrow-function-2.js
@@ -1,21 +1,23 @@
-'use strict';
-
-var _util = require('util');
-
+import { print } from "util";
+  
 //Arguments object - no longer bound with arrow functions
 
-var add = function add(a, b) {
-    return a + b;
+const add  =  (a, b) => {
+    return a +b;
 };
+
 
 console.log(add(55, 1, 1001));
 
+
+
+
 //This keyword - no longer bound
 
-var user = {
+const user = {
     name: 'Jon',
     cities: ['Philadelphia', 'New York', 'Dublin'],
-    printPlacesLived: function printPlacesLived() {
+    printPlacesLived: function () {
         console.log(this.name);
         console.log(this.cities);
     }
