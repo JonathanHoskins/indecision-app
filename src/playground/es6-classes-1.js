fe1e1351778acb11d273dc7d0a67b1179a309458
Hoskins <ACR14@CORP.UPC>
2018-05-07 16:06:37 -0400

    Added Traveler extended class.

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -12,8 +12,58 @@ class Person {
     }
 }
 
-const me = new Person('Jon Hoskins', 30);
+class Student extends Person {
+    constructor(name, age, major){
+        super(name, age);
+        this.major = major;
+    }
+
+    hasMajor(){
+        return !!this.major;
+    }
+
+    getDescription(){
+        let description = super.getDescription();
+
+        if(this.hasMajor()){
+            description += ` Their major is ${this.major}`;
+        }
+
+        return description;
+    }
+}
+
+//Traveler -> Person
+
+class Traveler extends Person {
+    constructor(name, age, homeLocation){
+        super(name, age);
+        this.homeLocation = homeLocation;
+    }
+
+    hasHomeLocation(){
+        return !!this.homeLocation;
+    }
+
+    getDescription(){
+        let description = super.getDescription();
+
+        if(this.hasHomeLocation())
+        {
+            description += ` Their location is ${this.homeLocation}`;
+        }
+
+        return description;
+    }
+}
+
+//Add support for homeLocation
+//Override getGreeting
+//1. Hi. I am Jon Hoskins. I'm visiting from Chattanooga
+//2. Hi. I am Jon Hoskins
+
+const me = new Traveler('Jon Hoskins', 30, 'Chattanooga');
 console.log(me.getDescription());
 
-const other = new Person();
+const other = new Traveler();
 console.log(other.getDescription());
